f459c232c834caa845e67343fc7ba9a99e32005b
Jacob Wu-Chen <97861475+jakewc12@users.noreply.github.com>
2026-04-08 22:28:40 -0400

    WAIT-4 Update prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,17 +1,10 @@
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
-import { PrismaPg } from "@prisma/adapter-pg";
 
 export default defineConfig({
-  earlyAccess: true,
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
-  },
-  migrate: {
-    async adapter() {
-      return new PrismaPg({ connectionString: process.env["DIRECT_URL"] });
-    },
   },
   datasource: {
     url: process.env["DIRECT_URL"],
